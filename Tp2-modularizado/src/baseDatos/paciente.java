d2150353f3088d101e23b27e12e3bd3013432ff8
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-06-05 16:28:51 -0300

    Update paciente.java

--- Tp2-modularizado/src/baseDatos/paciente.java
+++ Tp2-modularizado/src/baseDatos/paciente.java
@@ -12,7 +12,7 @@ public class paciente {
 		String op = "";
 		DataOutputStream datopac = null;
 		try {
-			datopac = new DataOutputStream(new FileOutputStream("C:\\datopac.txt"));
+			datopac = new DataOutputStream(new FileOutputStream("C:\\datopac.txt"));//abrimos el archivo para agregar pacientes
 			try {
 				do {
 					menu.ps("   ..............................................." + "\n");
@@ -20,19 +20,19 @@ public class paciente {
 					menu.ps("   :-:.........................................:-:" + "\n");
 
 					menu.ps("Digite el codigo del paciente: ");
-					codigoPaciente = menu.LeerCadena();
+					codigoPaciente = menu.LeerCadena(); //se ingresa el codigo del paciente 
 					datopac.writeUTF(codigoPaciente);
 					menu.ps("Digite el nombre del paciente: ");
-					nombrePaciente = menu.LeerCadena();
+					nombrePaciente = menu.LeerCadena(); // se ingresa Nombre y apellido del paciente
 
-					datopac.writeUTF(nombrePaciente);
+					datopac.writeUTF(nombrePaciente);// se escribe en el archivo datopac
 
-					menu.ps("Desea ingresar otro paciente? S/N" + "\n");
+					menu.ps("Desea ingresar otro paciente? S/N" + "\n");//pregunta en pantalla
 
-					op = menu.LeerCadena();
+					op = menu.LeerCadena();//leemos esa opcion que ingreso por pantalla
 
-				} while (op.equals("S") || op.equals("s"));
-				datopac.close();
+				} while (op.equals("S") || op.equals("s"));// si es no , salimos del while
+				datopac.close();//cerramos el archivo de texto de pacientes
 
 			} catch (IOException ioe) {
 			}
@@ -43,11 +43,12 @@ public class paciente {
 	}
 	
 	
+	
 	public void ingresoDeSituacionDelPaciente () {// ingreso de situacion del paciente
 		
 		DataOutputStream situpac = null;
 		try {
-			situpac = new DataOutputStream(new FileOutputStream("C:\\situpac.txt"));
+			situpac = new DataOutputStream(new FileOutputStream("C:\\situpac.txt"));//abrimos archivo de texto de situacion de paciente
 			String op = "";
 			do {
 				String codMedico="";
@@ -56,20 +57,20 @@ public class paciente {
 				menu.ps("   :-:...............................................:-:" + "\n");
 
 				menu.ps("Digite el codigo del paciente: ");
-				codigoPaciente = menu.LeerCadena();
-				situpac.writeUTF(codigoPaciente);
+				codigoPaciente = menu.LeerCadena();//ingreso codigo del paciente ya registrado
+				situpac.writeUTF(codigoPaciente);//lo escribo en el archivo
 				menu.ps("Digite el codigo del medico: ");
-				codMedico = menu.LeerCadena();
+				codMedico = menu.LeerCadena();//ingreso el codigo del medico que atiende al paciente
 				situpac.writeUTF(codMedico);
 				menu.ps("Digite el diagnostico del medico: ");
-				enfermedad = menu.LeerCadena();
+				enfermedad = menu.LeerCadena();//escribo diagnostico del paciente
 				situpac.writeUTF (enfermedad);
 
-				menu.ps("Desea ingresar otro registro al historial? S/N");
+				menu.ps("Desea ingresar otro registro al historial? S/N");//pregunta por pantalla
 				menu.ps("\n");
-				op = menu.LeerCadena();
+				op = menu.LeerCadena();//leo la respuesta a la pregunta
 
-			} while (op.equals("S") || op.equals("s"));
+			} while (op.equals("S") || op.equals("s"));//si es N, salgo del while
 			situpac.close();
 		} catch (IOException ioe) {
 		}
