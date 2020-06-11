1322e8dc1c168f99ddd0a76a201a05bebb6514c9
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-06-11 20:12:06 -0300

    Update menu.java

--- Tp2-modularizado/src/baseDatos/menu.java
+++ Tp2-modularizado/src/baseDatos/menu.java
@@ -5,42 +5,43 @@ import java.io.InputStreamReader;
 public class menu {
 	
 	public static void ps(String x) {//muestra por pantalla mensajes
-		System.out.print(x);
+		System.out.print(x);//imprime en pantalla
 	}
 
 	public static int LeerEntero() {//lee la opcion elegida en el menu
-		String línea = new String("");
+		String lÃ­nea = new String("");
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			línea = br.readLine();////leo linea ingresada
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//para leer la entrada por teclado
+			lÃ­nea = br.readLine();////leo linea ingresada
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		int ne = 0;
 		try {
-			ne = Integer.parseInt(línea);
+			ne = Integer.parseInt(lÃ­nea);//paso el sting a entero
 		} catch (Exception e) {
 		}
 		;
-		return (ne);
+		return (ne);//retorno el entero
 	}
 
 	public static String LeerCadena() {//lee la informacion ingresada en el ingreso de datos o consultas de informes
-		String línea = new String("");
+		String lÃ­nea = new String("");
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			línea = br.readLine();//leo linea ingresada
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//para leer la entrada por teclado
+			lÃ­nea = br.readLine();//leo linea ingresada
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		double ne = 0;
-		return (línea);
+		return (lÃ­nea);//retorno la cadena de texto
 	}
 	
 	
 	public static int MostrarMenuInicial() {//muestra menu principal del centro asistencial los pinares
-		int op1=0;
+		int op1=0;//entero para elegir la opcion 
 		do{
+			//Lineas impresas por pantalla 
 			menu.ps("   .............................................." + "\n");
 			menu.ps("   :-:         CENTRO ASISTENCIAL             :-:" + "\n");
 			menu.ps("   :-:   >>>> L O S  P I N A R E S   <<<<     :-:" + "\n");
@@ -53,8 +54,8 @@ public class menu {
 			menu.ps("   ....Elija la opcion deseada: ");
 			// ps("\n");
 			op1 = menu.LeerEntero();//leo la opcion elegida
-			if (op1 < 1 || op1 > 3) {
-				ps("Debe digitar una opcion del menu" + "\n");
+			if (op1 < 1 || op1 > 3) {//si no es correcta la opcion muestro mensaje para que vuelva ingresar una opcion
+				ps("Debe digitar una opcion del menu" + "\n");//mensaje
 				}
 		} while (op1 > 3 && op1 < 1);//si es una opcion valida, salgo sino vuelve a elegirla
 		return op1;//retorno la opcion valida elegida
@@ -62,8 +63,9 @@ public class menu {
 	
 	
 	public static int MostrarMenuIngresoDatos() {//muestra el menu para el ingreso de datos
-		int op2=0;
+		int op2=0;//entero para elegir la opcion
 		do {
+			//Lineas impresas por pantalla 
 			ps("   ..............................................." + "\n");
 			ps("   :-: -I N G R E S O  D E  P A C I E N T E S- :-:" + "\n");
 			ps("   :-:.........................................:-:" + "\n");
@@ -76,8 +78,8 @@ public class menu {
 
 			op2 = LeerEntero();//leo la opcion puesta
 
-			if (op2 < 1 || op2 > 4) {
-				ps("Debe digitar una opcion del menu" + "\n");
+			if (op2 < 1 || op2 > 4) {//si no es correcta la opcion muestro mensaje para que vuelva ingresar una opcion
+				ps("Debe digitar una opcion del menu" + "\n");//mensaje
 			}
 		} while (op2 < 1 || op2 > 4);//si es una opcion valida, salgo sino vuelve a elegirla
 		return op2;//retorno la opcion valida elegida
@@ -85,8 +87,9 @@ public class menu {
 	
 	
 	public static int MostrarMenuInformes() {// muestra menu para la consulta de informes
-		int op2=0;
+		int op2=0;//entero para elegir la opcion
 		do {
+			//Lineas impresas por pantalla 
 			ps("   ..............................................." + "\n");
 			ps("   :-:  C O N T R O L  D E  P A C I E N T E S  :-:" + "\n");
 			ps("   ..............................................." + "\n");
@@ -98,8 +101,8 @@ public class menu {
 			ps("   ..............................................." + "\n");
 			ps("   ....Elija la opcion deseada: ");
 			op2 = LeerEntero();//leo la opcion elegida
-			if (op2 < 1 || op2 > 3) {
-				ps("Seleccione una de las opciones del menu" + "\n");
+			if (op2 < 1 || op2 > 3) {//si no es correcta la opcion muestro mensaje para que vuelva ingresar una opcion
+				ps("Seleccione una de las opciones del menu" + "\n");//mensaje
 			}
 		} while (op2 < 1 || op2 > 3);//si es valida la opcion salgo sino sigo
 		return op2;//retorno la opcion valida elegida
@@ -107,3 +110,4 @@ public class menu {
 	
 	
 }
+
