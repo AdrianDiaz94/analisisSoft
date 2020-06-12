67000745c10116361d4481c3b1de41f37e488dbb
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-06-12 02:37:39 -0300

    Update bdatoa.java

--- Tp2-modularizado/src/baseDatos/bdatoa.java
+++ Tp2-modularizado/src/baseDatos/bdatoa.java
@@ -9,37 +9,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class bdatoa {
-	/*********************** MAIN *********************************/
 	public static void main(String args[]) throws Exception {
-		int op1, op2, op3; // variables de selección usadas en los diferentes menús
+		int op1, op2, op3; // variables de selecciÃ³n usadas en los diferentes menÃºs
 		
 		do {
 			op1 = menu.MostrarMenuInicial();
 			
-			if (op1 == 1) {// seleción ingreso de datos
+			if (op1 == 1) {// seleciÃ³n ingreso de datos
 				do {
 					op2 = menu.MostrarMenuIngresoDatos();
-					paciente p= new paciente();
-					medico m= new medico();
+					paciente p= new paciente();//crea un nuevo paciente
+					medico m= new medico();//crea un nuevo medico
 					switch (op2) {
-					case 1: // ingreso de datos, datos del paciente
-						p.ingresoDeDatos();
+					case 1: // datos del paciente
+						p.ingresoDeDatos();//ingreso de pacientes
 						break;
 					
-					case 2: // ingreso de datos, situacion del paciente
-						p.ingresoDeSituacionDelPaciente();
+					case 2: // situacion del paciente
+						p.ingresoDeSituacionDelPaciente();//ingreso de situacion de paciente
 						break;
 
-					case 3: //ingreso de datos, datos del medico
-						m.AgregarInfoMedico();
+					case 3: //datos del medico
+						m.AgregarInfoMedico();//ingreso de datos del medico
 					}
 			} while (op2 != 4);
 			} else {
-				if (op1 == 2) // seleción informes
+				if (op1 == 2) // seleciÃ³n informes
 				{
 					do {
-					medico m= new medico();
-					op2 = menu.MostrarMenuInformes();
+					medico m= new medico();//creo medico nuevo
+					op2 = menu.MostrarMenuInformes();//muestro por pantalla el menu y espera la opcion elegida
 					m.informesMedicos(op2); // informe listado de pacientes o enfermedades que atiende
 						
 					} while (op2 != 3);
